b7f0437ab7920abdd4f38521c0eebdff57aa0440
Anand Srinivasan <anand@a0s.co>
2024-10-10 14:47:57 +0100

    2d rois

--- src/microseg/widgets/pg.py
+++ src/microseg/widgets/pg.py
@@ -16,7 +16,7 @@ import microseg.utils.mask as mutil
 from microseg.utils.colors import *
 from microseg.utils.data import *
 from matgeo import Sphere
-from .rois_2d import *
+from .rois import *
 
 '''
 Functions
@@ -306,193 +306,6 @@ class MaskImageWidget(NoTouchPlotWidget):
         self._img_item.setImage(img)
         self._mask_item.setMask(mask)
 
-class ThingsImageWidget(ImagePlotWidget, metaclass=QtABCMeta):
-    '''
-    Editable widget for drawing things on an image
-    '''
-    edited = QtCore.Signal()
-    undo_n: int=100
-
-    def __init__(self, editable: bool=False, **kwargs):
-        super().__init__(**kwargs)
-        self._editable = editable
-
-        # State
-        self._things: List[LabeledThing] = []
-        self._items: List[SelectableItem] = []
-        self._selected = []
-        self._shortcuts = []
-        self._vb = None
-        self._reset_drawing_state()
-        self._next_label: int = None
-        self._undo_stack = None # Uninitialized
-        self._redo_stack = None
-
-        if editable:
-            self.add_sc('Delete', lambda: self._delete())
-            self.add_sc('Backspace', lambda: self._delete())
-            self.add_sc('E', lambda: self._edit())
-            self.add_sc('Ctrl+Z', lambda: self._undo())
-            self.add_sc('Ctrl+Y', lambda: self._redo())
-            self.add_sc('Ctrl+Shift+Z', lambda: self._redo())
-        self.add_sc('Escape', lambda: self._escape())
-        self.scene().sigMouseMoved.connect(self._mouse_move)
-
-    def add_sc(self, key: str, fun: Callable):
-        sc = QShortcut(QKeySequence(key), self)
-        sc.activated.connect(fun)
-        self._shortcuts.append(sc)
-
-    def setImage(self, img: np.ndarray):
-        self._img.setImage(img)
-
-    @abc.abstractmethod
-    def createItemFromThing(self, thing: LabeledThing) -> SelectableItem:
-        pass
-
-    @abc.abstractmethod
-    def getThingFromItem(self, item: SelectableItem) -> LabeledThing:
-        pass
-
-    def setThings(self, things: List[LabeledThing], reset_stacks: bool=True):
-        # Remove previous things
-        self._selected = []
-        self._things = things
-        for item in self._items:
-            self.removeItem(item)
-        # Add new things
-        self._items = []
-        for i, thing in enumerate(things):
-            item = self.createItemFromThing(thing)
-            self.addItem(item)
-            self._listenItem(i, item)
-            self._items.append(item)
-        if reset_stacks:
-            self._undo_stack = [self.getThings()]
-            self._redo_stack = []
-        self._reset_drawing_state()
-
-    def getThings(self) -> List[LabeledThing]:
-        return [t.copy() for t in self._things]
-
-    def _listenItem(self, i: int, item: SelectableItem):
-        item.sigClicked.connect(lambda: self._select(i))
-
-    def _select(self, i: Optional[int]):
-        if i is None:
-            self._unselect_all()
-        if not i in self._selected:
-            if not QGuiApplication.keyboardModifiers() & Qt.ShiftModifier:
-                self._unselect_all()
-            self._selected.append(i)
-            self._items[i].select()
-        print(f'Selected: {self._selected}')
-
-    def _unselect_all(self):
-        for i in self._selected:
-            self._items[i].unselect()
-        self._selected = []
-
-    def _delete(self):
-        if self._editable and len(self._selected) > 0:
-            print(f'delete {len(self._selected)} things')
-            for i in self._selected:
-                self.removeItem(self._items[i])
-            selset = set(self._selected)
-            self._things = [t for i, t in enumerate(self._things) if not i in selset]
-            self._items = [t for i, t in enumerate(self._items) if not i in selset]
-            self._selected = []
-            self._push_stack()
-
-    def _edit(self):
-        print('edit')
-        if self._editable and not self._is_drawing:
-            self._select(None)
-            self._is_drawing = True
-            assert not self._next_label is None, 'No next label'
-            print('Next label:', self._next_label)
-            self._drawn_lbl = self._next_label
-            self._vb = self.getViewBox()
-            self._vb.setMouseEnabled(x=False, y=False)
-
-    def _escape(self):
-        print('escape')
-        if not self._drawn_item is None:
-            self.removeItem(self._drawn_item)
-        self._reset_drawing_state()
-        self._select(None)
-
-    @abc.abstractmethod
-    def initDrawingState(self):
-        ''' Reset the drawing state specific to this item '''
-        pass
-
-    def _reset_drawing_state(self):
-        self.initDrawingState()
-        self._is_drawing = False
-        self._drawn_lbl = None
-        self._drawn_item = None
-        if not self._vb is None:
-            self._vb.setMouseEnabled(x=True, y=True)
-        self._vb = None
-
-    @abc.abstractmethod
-    def modifyDrawingState(self, pos):
-        ''' Modify the drawing state from given pos specific to this item '''
-        pass
-
-    @abc.abstractmethod
-    def finishDrawingState(self):
-        ''' Finish the drawing state specific to this item '''
-        pass
-
-    def _mouse_move(self, pos):
-        if self._is_drawing:
-            if QtCore.Qt.LeftButton & QtWidgets.QApplication.mouseButtons():
-                pos = self._vb.mapSceneToView(pos)
-                pos = np.array([pos.x(), pos.y()])
-                self.modifyDrawingState(pos)
-            else:
-                if not self._drawn_item is None:
-                    print('ending draw')
-                    self.finishDrawingState()
-                    thing = self.getThingFromItem(self._drawn_item)
-                    self._things.append(thing)
-                    self._items.append(self._drawn_item)
-                    N = len(self._things)-1
-                    self._listenItem(N, self._drawn_item)
-                    self._reset_drawing_state()
-                    self._push_stack()
-                    # self._select(N)
-
-    def _push_stack(self):
-        self._undo_stack.append(self.getThings())
-        self._undo_stack = self._undo_stack[-self.undo_n:]
-        self._redo_stack = []
-        print(f'Current stacks: undo {len(self._undo_stack)}, redo {len(self._redo_stack)}')
-        self.edited.emit()
-
-    def _undo(self):
-        print('undo')
-        if len(self._undo_stack) > 1:
-            self._redo_stack.append(self._undo_stack[-1])
-            self._undo_stack = self._undo_stack[:-1]
-            things = [t.copy() for t in self._undo_stack[-1]]
-            self.setThings(things, reset_stacks=False)
-            self.edited.emit()
-        else:
-            print('Cannot undo further')
-
-    def _redo(self):
-        print('redo')
-        if len(self._redo_stack) > 0:
-            self._undo_stack.append(self._redo_stack[-1])
-            self._redo_stack = self._redo_stack[:-1]
-            things = [t.copy() for t in self._undo_stack[-1]]
-            self.setThings(things, reset_stacks=False)
-            self.edited.emit()
-        else:
-            print('Cannot redo further')
 
 class CirclesImageWidget(ThingsImageWidget):
     '''
@@ -611,9 +424,9 @@ class ThingSegmentorWidget(SaveableWidget, metaclass=QtABCMeta):
         # State
         self._z = 0
         self._img: np.ndarray = None
-        self._things: List[LabeledThing] = []
+        self._things: List[LabeledROI] = []
 
-    def setData(self, img: np.ndarray, things: List[LabeledThing]=[]):
+    def setData(self, img: np.ndarray, things: List[LabeledROI]=[]):
         self._img = img
         self._things = things
         self._setZ(0)
@@ -652,7 +465,7 @@ class ThingSegmentorWidget(SaveableWidget, metaclass=QtABCMeta):
         self._nthings.setText(f'Things: {len(self._things)}')
         print('things edited')
 
-    def getData(self) -> List[LabeledThing]:
+    def getData(self) -> List[LabeledROI]:
         return self._things
     
 class ThingsSegmentorWindow(MainWindow):
@@ -688,7 +501,7 @@ class ThingsSegmentorWindow(MainWindow):
         # Listeners
         self._seg.saved.connect(self._save)
 
-    def _toPyQtGraph(self, things: List[LabeledThing]):
+    def _toPyQtGraph(self, things: List[LabeledROI]):
         '''
         Transform things appropriately for rendering in pyqtgraph's weird orientation defaults
         '''
@@ -696,7 +509,7 @@ class ThingsSegmentorWindow(MainWindow):
         things = [(t + offset).flipy(self._ylen) for t in things]
         return things
     
-    def _fromPyQtGraph(self, things: List[LabeledThing]):
+    def _fromPyQtGraph(self, things: List[LabeledROI]):
         ''' 
         Transform things appropriately for rendering in pyqtgraph's weird orientation defaults
         '''
---
+++ src/microseg/widgets/rois.py
@@ -0,0 +1,103 @@
+''' 
+2D ROIs
+'''
+
+from typing import Tuple, List, Callable
+import numpy as np
+from qtpy import QtCore
+from qtpy.QtWidgets import QGraphicsPolygonItem, QGraphicsRectItem, QGraphicsEllipseItem, QGraphicsItem
+import pyqtgraph as pg
+from abc import ABC, abstractmethod
+
+from matgeo.plane import PlanarPolygon
+from matgeo.ellipsoid import Circle, Ellipsoid, Ellipse
+
+from .base import *
+from microseg.utils import pg_colors
+
+'''
+ROIs
+'''
+
+ROI = Union[PlanarPolygon, Ellipse, Circle]
+
+class LabeledROI:
+    def __init__(self, lbl: int, roi: ROI):
+        self.lbl = lbl
+        self.roi = roi
+
+    def copy(self) -> 'LabeledROI':
+        return LabeledROI(self.lbl, self.roi.copy())
+
+    def flipy(self, yval: float) -> 'LabeledROI':
+        ''' This is needed because of Pyqtgraph's insane orientation defaults '''
+        return LabeledROI(self.lbl, self.roi.flipy(yval))
+
+    def __add__(self, offset: np.ndarray):
+        ''' This is needed because of Pyqtgraph's insane orientation defaults '''
+        return LabeledROI(self.lbl, self.roi + offset)
+
+'''
+ROI Items (for adding to Qt widgets)
+'''
+
+class SelectableROIItem:
+    def __init__(self, lroi: LabeledROI, selectable: bool=True):
+        self._lroi = lroi
+        self._pen = pg_colors.cc_pens[lroi.lbl % pg_colors.n_pens]
+        self._hpen = pg_colors.cc_pens_hover[lroi.lbl % pg_colors.n_pens]
+        self._selected = False
+        self.setPen(self._pen)
+        if selectable:
+            self._proxy = ClickProxy()
+            self.sigClicked = self._proxy.sigClicked
+            self.setAcceptHoverEvents(True)
+
+    def hoverEnterEvent(self, event):
+        self.setPen(self._hpen)
+
+    def hoverLeaveEvent(self, event):
+        if not self._selected:
+            self.setPen(self._pen)
+
+    def mousePressEvent(self, event):
+        self.sigClicked.emit()
+        self.select()
+
+    def select(self):
+        self._selected = True
+        self.setPen(self._hpen)
+
+    def unselect(self):
+        self._selected = False
+        self.setPen(self._pen)
+
+class SelectablePolygonItem(QGraphicsPolygonItem, SelectableROIItem):
+    def __init__(self, lroi: LabeledROI, *args, **kwargs):
+        # Both constructors get called automatically, so pass the named one explicitly
+        super().__init__(pg.QtGui.QPolygonF([
+            QtCore.QPointF(p[0], p[1]) for p in lroi.vertices
+        ]), *args, lroi=lroi, **kwargs)
+
+class SelectableEllipseItem(QGraphicsEllipseItem, SelectableROIItem):
+    def __init__(self, lroi: LabeledROI, *args, **kwargs):
+        # Both constructors get called automatically, so pass the named one explicitly
+        super().__init__(*args, lroi=lroi, **kwargs)
+        P, rs = lroi.get_axes_stretches()
+        hr, vr = rs
+        x, y = lroi.v
+        self.setRect(x-hr, y-vr, 2*hr, 2*vr)
+        P = P.T
+        theta = np.arctan2(P[1,0], P[0,0]) # Angle from orthogonal matrix P.T
+        self.setTransformOriginPoint(QtCore.QPointF(x+hr, y+vr)) # Transform about ellipse center
+        self.setRotation(theta*180/np.pi) # Rotate onto basis P
+
+class SelectableCircleItem(QGraphicsEllipseItem, SelectableROIItem):
+    def __init__(self, lroi: LabeledROI, *args, **kwargs):
+        # Both constructors get called automatically, so pass the named one explicitly
+        super().__init__(*args, lroi=lroi, **kwargs) 
+        self.setRadius(lroi.r)
+
+    def setRadius(self, r: float):
+        self.setRect(self._lroi.v[0]-r, self._lroi.v[1]-r, 2*r, 2*r)
+        self._lroi.r = r
